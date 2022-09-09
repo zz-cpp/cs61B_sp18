802164a4875c6543c378cc0df50a6d4bc40f6e7a
z.z <1318781143@qq.com>
2022-09-09 10:48:28 +0800

    project1gold modify 05

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -16,64 +16,62 @@ public class TestArrayDequeGold {
         Integer exp;
         StudentArrayDeque<Integer> sd = new StudentArrayDeque();
         LinkedList<Integer> solve = new ArrayDequeSolution();
-
+        String message = "";
         int operation1;
         int operation2;
         int randomNum;
 
-        for (int j = 0; j < 1000; j++) {
-            String message = "";
-            for (int i = 0; i < 1000; i++) {
-                operation1 = StdRandom.uniform(0, 2);
-                randomNum = StdRandom.uniform(0, 100);
-                if (operation1 == 1) {
-                    sd.addFirst(randomNum);
-                    solve.addFirst(randomNum);
-                    message += "addFirst(" + randomNum + ")" + "\n";
-                } else {
-                    sd.addLast(randomNum);
-                    solve.addLast(randomNum);
-                    message += "addLast(" + randomNum + ")\n";
+
+        for (int i = 0; i < 1000; i++) {
+            operation1 = StdRandom.uniform(0, 2);
+            randomNum = StdRandom.uniform(0, 100);
+            if (operation1 == 1) {
+                sd.addFirst(randomNum);
+                solve.addFirst(randomNum);
+                message += "addFirst(" + randomNum + ")" + "\n";
+            } else {
+                sd.addLast(randomNum);
+                solve.addLast(randomNum);
+                message += "addLast(" + randomNum + ")\n";
+            }
+
+            operation2 = StdRandom.uniform(0, 4);
+            switch (operation2) {
+                case 0: {
+                    actBoolean = sd.isEmpty();
+                    expBoolean = solve.isEmpty();
+                    if (actBoolean != expBoolean) {
+                        message += "isEmpty() " + actBoolean + "\n";
+                    }
+                    assertEquals(expBoolean, actBoolean);
                 }
+                break;
 
-                operation2 = StdRandom.uniform(0, 4);
-                switch (operation2) {
-                    case 0: {
-                        actBoolean = sd.isEmpty();
-                        expBoolean = solve.isEmpty();
-                        if (actBoolean != expBoolean) {
-                            message += "isEmpty() " + actBoolean + "\n";
-                        }
-                        assertEquals(expBoolean, actBoolean);
+                case 1: {
+                    act = sd.size();
+                    exp = solve.size();
+                    if (!act.equals(exp)) {
+                        message += "size() " + act + "\n";
                     }
-                    break;
-
-                    case 1: {
-                        act = sd.size();
-                        exp = solve.size();
-                        if (!act.equals(exp)) {
-                            message += "size() " + act + "\n";
-                        }
-                        assertEquals(exp, act);
-                    }
-                    break;
-
-                    case 2: {
-                        act = sd.removeFirst();
-                        exp = solve.removeFirst();
-                        message += "removeFirst(): " + act + "\n";
-                        assertEquals(message, exp, act);
-                    }
-                    break;
-
-                    case 3: {
-                        act = sd.removeLast();
-                        exp = solve.removeLast();
-                        message += "removeLast(): " + act + "\n";
-                        assertEquals(message, exp, act);
-                    }
-                    default:
+                    assertEquals(exp, act);
                 }
+                break;
+
+                case 2: {
+                    act = sd.removeFirst();
+                    exp = solve.removeFirst();
+                    message += "removeFirst(): " + act + "\n";
+                    assertEquals(message, exp, act);
+                }
+                break;
+
+                case 3: {
+                    act = sd.removeLast();
+                    exp = solve.removeLast();
+                    message += "removeLast(): " + act + "\n";
+                    assertEquals(message, exp, act);
+                }
+                default:
             }
         }
     }
